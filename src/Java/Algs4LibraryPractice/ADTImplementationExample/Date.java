b3b2df7096fba784bdf8e56fee4c01fcfc4a3466
Doyoon Kim <doyoon3312@kakao.com>
2021-05-26 17:54:15 +0900

    Commit [052621] - equals() method implementation.
    * equals() method implementation example. (Date.java)

--- src/Java/Algs4LibraryPractice/ADTImplementationExample/Date.java
+++ src/Java/Algs4LibraryPractice/ADTImplementationExample/Date.java
@@ -35,6 +35,40 @@ public class Date {
         return month() + "/" + day() + "/" + year();
     }
 
+    // Overridden equals() method.
+    /*
+        Equality: Java's convention is that equals() must be an equivalence relation. It must be:
+        " Take an Object as an argument.
+        1. Reflexive: x.equals(y) is true.
+        2. Symmetric: x.equals(y) is true, if and only if y.equals(x) is true.
+        3. Transitive: x.equals(y) and y.equals(z) are true, then so is x.equals(z).
+        4. Consistent: Multiple invocations of x.equals(y) consistently return the same value, provided neither object
+        is modified.
+        5. Not Null
+     */
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        } else {
+            if (this.getClass() == obj.getClass()) {
+                return true;
+            }
+            Date target = (Date) obj;
+            if (this.day == target.day) {
+                return true;
+            }
+            if (this.month == target.month) {
+                return true;
+            }
+            if (this.year == target.year) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     // Test Client
     public static void main(String[] args) {
         int month = Integer.parseInt(args[0]);
@@ -42,6 +76,17 @@ public class Date {
         int year = Integer.parseInt(args[2]);
 
         Date date = new Date(month, day, year);
+        Date test1 = new Date(5, 26, 2021);
+        Date date2 = date;
         System.out.println("General Implementation:\n" + date.toString());
+
+        System.out.println(date == test1);
+        System.out.println(date == date2);
+        System.out.println(date2 == test1);
+
+        System.out.println("EQUALS METHOD");
+        System.out.println(date.equals(test1));
+        System.out.println(date.equals(date2));
+        System.out.println(date2.equals(test1));
     }
 }
