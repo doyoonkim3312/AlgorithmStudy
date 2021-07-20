2dea662afb088ca5e4b52429535774c47118a54e
Doyoon Kim <doyoon3312@kakao.com>
2021-07-20 23:51:25 +0900

    Commit[072021] - Suspend Function (continued)
    * Lazily started function
    
    * Async-Style Function using GlobalScope. (This example is strongly discouraged in Kotlin Coroutine.)
    
    * Structured Concurrency with async{...}: coroutineScope (appropriately confined instance of CoroutineScope)

--- src/Coroutines/CoroutinePractice_03.kt
+++ src/Coroutines/CoroutinePractice_03.kt
@@ -2,14 +2,17 @@ package coroutines
 
 /*
     July 19, 2021 - Sequential, Concurrent
+    July 20, 2021 - lazily started, async-style function, structured concurrency
  */
 
 import kotlinx.coroutines.*
-import kotlin.system.measureTimeMillis
+import kotlin.system.*
 
 fun main() {
     // sequentialByDefaultExample()
-    simpleConcurrentExample()   // Faster than invoking two functions sequentially.
+    // simpleConcurrentExample()   // Faster than invoking two functions sequentially.
+    // lazilyStartedAsync()
+    structuredConcurrencyExample()
 }
 
 suspend fun doSomethingUsefulOne(): Int {
@@ -21,6 +24,43 @@ suspend fun doSomethingUsefulTwo(): Int {
     delay(1000L)
     return 29
 }
+
+// Structured Concurrency
+suspend fun concurrentSum(): Int = coroutineScope {
+    val first = async {
+        doSomethingUsefulOne()
+    }
+    val second = async {
+        doSomethingUsefulTwo()
+    }
+    first.await() + second.await()
+}
+
+/*
+The code below is strongly discouraged in Kotlin Coroutine.
+
+fun doSomethingUsefulOneAsync() = GlobalScope.async {
+    doSomethingUsefulOne()
+}
+
+fun doSomethingUsefulTwoAsync() = GlobalScope.async {
+    doSomethingUsefulTwo()
+}
+
+fun asyncStyleFunction() {
+    val time = measureTimeMillis {
+        val first = doSomethingUsefulOneAsync()
+        val second = doSomethingUsefulTwoAsync()
+
+        // Main function dose not have runBlocking, however, waiting for a result must involve either
+        // suspend function or blocking.
+        runBlocking {
+            println("Result: ${first.await() + second.await()}")
+        }
+    }
+    println("TASK COMPLETED in $time ms")
+}
+ */
 
 fun sequentialByDefaultExample() = runBlocking {
     val time = measureTimeMillis {
@@ -40,6 +80,33 @@ fun simpleConcurrentExample() = runBlocking {
             doSomethingUsefulTwo()
         }
         println("Result: ${first.await() + second.await()}")
+    }
+    println("TASK COMPLETED in $time ms")
+}
+
+fun lazilyStartedAsync() = runBlocking {
+    val time = measureTimeMillis {
+        // Each coroutines will be started if .start() function is invoked, or its deferred value is required by
+        // .await() function.
+
+        val first = async (start = CoroutineStart.LAZY) {
+            // println("DO SOMETHING USEFUL ONE")
+            doSomethingUsefulOne()
+        }
+        val second = async (start = CoroutineStart.LAZY) {
+            // println("DO SOMETHING USEFUL TWO")
+            doSomethingUsefulTwo()
+        }
+        first.start()   // this line starts coroutine, and return deferred value to value 'first'
+        second.start()
+        println("Result: ${first.await() + second.await()}")
+    }
+    println("TASK COMPLETED in $time ms")
+}
+
+fun structuredConcurrencyExample() = runBlocking {
+    val time = measureTimeMillis {
+        println("Result: ${concurrentSum()}")
     }
     println("TASK COMPLETED in $time ms")
 }
