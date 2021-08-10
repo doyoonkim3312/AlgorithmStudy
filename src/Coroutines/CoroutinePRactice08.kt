3094fe6630ae4784961882dc054b10631f3e1ace
Doyoon Kim <doyoon3312@kakao.com>
2021-08-10 23:00:34 +0900

    Commit[081021] - Coroutine Exception Handling (continued)
    * CoroutineExceptionHandler: Basic concept of CoroutineExceptionHandler, and its primary use.
    
    * Cancellation and Exception: The way how exception is being handled under the nature of coroutines' Parent-Child hierarchy.

--- src/Coroutines/CoroutinePRactice08.kt
+++ src/Coroutines/CoroutinePRactice08.kt
@@ -3,9 +3,14 @@ package coroutines
 // Coroutine Exception Handling
 
 import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.flow.*
 
 fun main() {
-    exceptionHandlingUnderRootCoroutine()
+    // exceptionHandlingUnderRootCoroutine()
+    // coroutineExceptionHandlerExample()
+    // cancellationExceptionIgnoredByHandler()
+    exceptionHandlingUnderParentChildRelationship()
 }
 
 // Example of Exception Handling under ROOT coroutine.
@@ -23,6 +28,7 @@ fun exceptionHandlingUnderRootCoroutine() = runBlocking<Unit> {
                                     // user calls .await() or .awaitAll() )
     }
 
+
     // User consume deferredValue
     try {
         deferredValue.await()   // Exception will be caught and handled by try-catch statement.
@@ -32,3 +38,82 @@ fun exceptionHandlingUnderRootCoroutine() = runBlocking<Unit> {
     }
 }
 
+// CoroutineExceptionHandler.
+// On JVM, it is possible to redefine global exception handler for all coroutines by registering
+// CoroutineExceptionHandler via ServiceLoader.
+// CoroutineExceptionHandler is invoked only on uncaught exceptions - exceptions that were not handled in any other way.
+fun coroutineExceptionHandlerExample() = runBlocking<Unit> {
+    // Create new CoroutineExceptionHandler.
+    // CoroutineExceptionHandler { CoroutineContext, Throwable -> ... }
+    val exceptionHandler = CoroutineExceptionHandler { _, e ->
+        println("Coroutine Exception Handler got $e exception.")
+    }
+
+    // Root Coroutine (using launch builder)
+    val rootCoroutineReturnsJob = GlobalScope.launch(exceptionHandler) {
+        throw AssertionError()
+    }
+
+    // Root Coroutine (using async builder)
+    val rootCoroutineReturnsDeferred = GlobalScope.async(exceptionHandler) {
+        throw ArithmeticException() // Noting will be printed until user consume this coroutine.
+    }
+
+    joinAll(rootCoroutineReturnsJob, rootCoroutineReturnsDeferred)
+}
+
+// Cancellation and Exception.
+// Coroutine internally use CancellationException for cancellation, these exception are ignored by handlers, so they
+// should be used only as the source of additional debug information, which can be obtained by catch block.
+fun cancellationExceptionIgnoredByHandler() = runBlocking<Unit> {
+    // In this example, CoroutineExceptionHandler is always installed to a coroutine that is created in GlobalScope.
+    val parent = launch {
+        val child = launch {
+            try {
+                delay(Long.MAX_VALUE)
+            } finally {
+                println("This Child coroutine has been cancelled.")
+            }
+        }
+        yield() // Review: yield() - Periodically invoke suspend functions, that checks for cancellation.
+                // Child coroutine will start its extremely long computation.
+        println("Cancelling Child Coroutine.")
+        child.cancel()  // Internally using CancellationException, which is going to be ignored by handler.
+        yield()
+        println("Parent Coroutine is not cancelled.")   // If child coroutine encounters an exception other than
+                                                        // CancellationException, it cancels its parent coroutine
+                                                        // with that exception.
+    }
+    parent.join()
+}
+
+// Cancellation and Exception (2)
+fun exceptionHandlingUnderParentChildRelationship() = runBlocking {
+    val exceptionHandler = CoroutineExceptionHandler { _, e ->
+        println("Exception Handler got $e exception.")
+    }
+
+    val parent = GlobalScope.launch(exceptionHandler) {
+        // Launch its first child coroutine.
+        launch {
+            try {
+                delay(Long.MAX_VALUE)
+            } finally {
+                withContext(NonCancellable) {
+                    println("First Child coroutine has been cancelled, but exception is not handled until " +
+                            "all the children coroutines terminate")
+                    delay(1000L)
+                    println("The first Child coroutine completes its non cancellable block.")
+                }
+            }
+        }
+
+        // launch its second child coroutine.
+        launch {
+            delay(10L)
+            println("Second Child coroutine will throw ArithmeticException.")
+            throw ArithmeticException() // Exception will be handled until all the children coroutines terminate.
+        }
+    }
+    parent.join()
+}
